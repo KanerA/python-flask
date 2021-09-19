0d3465faf7049462da82ddb3801b58fcb5001b29
Assaf Kaner <kaner.assaf@gmail.com>
2021-09-19 13:53:25 +0300

    .

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-x = 'hello'
 
-@app.route('/')
+@app.route("/")
 def home():
     return "Home"
 
 
-
 if __name__ == "__main__":
     app.run()
-
