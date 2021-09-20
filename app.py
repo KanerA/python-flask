14eadd9fb06213d984cef01a29a0875e07bc3475
Assaf Kaner <kaner.assaf@gmail.com>
2021-09-20 17:39:24 +0300

    get .env variables

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ from flask_pymongo import PyMongo
 from dotenv import load_dotenv
 from bson.json_util import dumps
 
+load_dotenv()
 app = Flask(__name__)
+uri = os.getenv("MONGO_URI")
 
 
 @app.route("/")
